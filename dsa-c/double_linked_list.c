ac9dd5c56e898c7d878cdf69be077e623d97add0
Kevin B <93852813+retrobone@users.noreply.github.com>
2025-03-24 22:09:09 +0530

    Update double_linked_list.c

--- dsa-c/double_linked_list.c
+++ dsa-c/double_linked_list.c
@@ -1,4 +1,4 @@
-//22.03.2025
+//24.03.2025
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,12 +11,12 @@ typedef struct node
 
 void main()
 {
-    DLList *start, *temp, *newnode;
+    DLList *start, *temp, *newnode, *pretemp;
     int ans, loc, val, i;
     start = NULL;
     do
     {
-        printf("\n Double Linked List");
+        printf("\n Double Linked List - Written by Kevin B");
         printf("\n 1. Add node at beginning ");
         printf("\n 2. Add node at end");
         printf("\n 3. Add node at specific location");
@@ -31,14 +31,14 @@ void main()
         printf("\n 12. Display");
         printf("\n 13. Reverse display");
         printf("\n 14. Exit");
-        printf("\n What do you want to use :");
+        printf("\n What do you want to use : ");
         scanf("%d", &ans);
 
         switch (ans)
         {
         case 1: // Add node at beginning
             newnode = (DLList *)malloc(sizeof(DLList));
-            printf("\n Enter data :");
+            printf("\n Enter data : ");
             scanf("%d", &newnode->data);
             newnode->prev = NULL;
             newnode->next = start;
@@ -90,7 +90,101 @@ void main()
             }
             break;
 
-        case 12:
+        case 4: // Add node before specific value
+            newnode = (DLList *)malloc(sizeof(DLList));
+            printf("\n Enter data to insert : ");
+            scanf("%d", &newnode->data);
+            printf("\n Before which element do you want to insert : ");
+            scanf("%d", &val);
+            temp = start;
+            while (temp->data != val)
+            {
+                pretemp = temp;
+                temp = temp->next;
+            }
+            if (temp == start)
+            {
+                newnode->prev = NULL;
+                newnode->next = temp;
+                start = newnode;
+            }
+            else
+            {
+                newnode->next = pretemp->next;
+                newnode->prev = pretemp;
+                pretemp->next = newnode;
+                temp->prev = newnode;
+            }
+            break;
+
+        case 5: // Add node after specific value
+            newnode = (DLList *)malloc(sizeof(DLList));
+            printf("\n Enter data to insert : ");
+            scanf("%d", &newnode->data);
+            printf("\n After which element do you want to insert : ");
+            scanf("%d", &val);
+            temp = start;
+            while (temp->data != val)
+            {
+                temp = temp->next;
+            }
+            newnode->next = temp->next;
+            newnode->prev = temp;
+            temp->next = newnode;
+            break;
+
+        case 6: // Delete from beginning
+            start = start->next;
+            break;
+
+        case 7: // Delete from end
+            temp = start;
+            while (temp->next != NULL)
+            {
+                pretemp = temp;
+                temp = temp->next;
+            }
+            pretemp->next = NULL;
+            break;
+
+        case 8: // Delete a location
+            printf("\n Which location do you want to delete : ");
+            scanf("%d", &loc);
+            temp = start;
+            for (i = 1; i < (loc - 1); i++)
+            {
+                pretemp = temp;
+                temp = temp->next;
+            }
+            pretemp->next = temp->next;
+            break;
+
+        case 9: // Delete before specified value
+            printf("\n Before which element do you want to delete : ");
+            scanf("%d", &val);
+            temp = start;
+            while (temp->data != val)
+            {
+                pretemp = temp;
+                temp = temp->next;
+            }
+            pretemp->prev->next = pretemp->next;
+            temp->prev = pretemp->prev;
+            break;
+
+        case 10: // Delete after specified value
+            printf("\n After which element do you want to delete : ");
+            scanf("%d", &val);
+            temp = start;
+            while (temp->data != val)
+            {
+                temp = temp->next;
+            }
+            temp->next = temp->next->next;
+            temp->next->prev = temp;
+            break;
+
+        case 12: // Display linked list
             temp = start;
             while (temp != NULL)
             {
